0cb39e54229ec713f6ca71215d1bbe5d21842250
John Barker <jdabarker@gmail.com>
2021-08-05 20:45:22 +0800

    deployed to heroku

--- server/server.js
+++ server/server.js
@@ -20,8 +20,6 @@ apolloServer.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "../client/public")));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
