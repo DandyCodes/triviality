bcc6a4ae4bbed4a8ea3b81283c3909440dc808e4
John Barker <jdabarker@gmail.com>
2021-08-05 20:39:28 +0800

    fixing static middleware

--- server/server.js
+++ server/server.js
@@ -20,15 +20,11 @@ apolloServer.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../client/public")));
+// app.use(express.static(path.join(__dirname, "../client/public")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
