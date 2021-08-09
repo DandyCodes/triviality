ae24629b083d4efabeee1450720c5b0be286610e
John Barker <jdabarker@gmail.com>
2021-08-09 11:07:04 +0800

    refactor apollo server config

--- server/config/apollo.js
+++ server/config/apollo.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("../schemas");
-const { authMiddleware } = require("../utils/auth");
+const { authMiddleware } = require("../utils/server-auth");
 
 const apollo = new ApolloServer({
   typeDefs,
