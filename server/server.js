270bf8e5d90c1cab6532d073b5cb5d52bcf3f4f3
John Barker <jdabarker@gmail.com>
2021-08-09 11:06:38 +0800

    refactor

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
-const path = require("path");
 const PORT = process.env.PORT || 3001;
-const mongooseConnection = require("./config/mongoose-connection");
-const apiRoutes = require("./controllers/api-routes");
-const apollo = require("./config/apollo");
+const path = require("path");
+
 const express = require("express");
 const app = express();
+
 const server = require("http").createServer(app);
+
 const io = require("socket.io")(server);
 require("./controllers/io-server")(io);
 
-apollo.applyMiddleware({ app });
+const mongooseConnection = require("./config/mongoose-connection");
 
+const apollo = require("./config/apollo");
+
+apollo.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
-
-app.use(apiRoutes);
 
 mongooseConnection.once("open", () => {
   server.listen(PORT, () => {
