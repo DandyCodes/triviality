95549c317f9afa44ecd521c9089877ac4347e89a
John Barker <jdabarker@gmail.com>
2021-08-09 11:13:23 +0800

    add login resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const { signToken } = require("../utils/server-auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Mutation: {
@@ -8,6 +9,19 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+  },
+
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new AuthenticationError("No user found with this email address");
+    }
+    const correctPw = await user.isCorrectPassword(password);
+    if (!correctPw) {
+      throw new AuthenticationError("Incorrect credentials");
+    }
+    const token = signToken(user);
+    return { token, user };
   },
 };
 
