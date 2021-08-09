14b4d7b6418fe6ee39fe01a457f322482079c485
John Barker <jdabarker@gmail.com>
2021-08-09 11:07:52 +0800

    add gui to test adding user

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,70 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 import { messageAll } from "./controllers/io-client";
-
-async function testApiRoute() {
-  const response = await fetch("/test");
-  console.log(response);
-}
+import clientAuth from "./utils/client-auth";
+import { ADD_USER } from "./utils/mutations";
 
 const msg = "hi";
 
 function App() {
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        username: formState.username,
+        email: formState.email,
+        password: formState.password,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    clientAuth.onLogin(token);
+  };
+
   return (
     <div className="App">
-      <button onClick={testApiRoute}>TEST API ROUTE</button>
       <button onClick={() => messageAll(msg)}>messageAll</button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          onChange={handleChange}
+        ></input>
+        <br></br>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <br></br>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+        ></input>
+        <br></br>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
